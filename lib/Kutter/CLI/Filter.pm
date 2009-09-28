c2b179beeb6e834edd84ead413561681a426c70d
Yusuke Wada <yusuke@kamawada.com>
2009-09-29 01:31:14 +0900

    add Filter Class to detect what to eat ?

---
+++ lib/Kutter/CLI/Filter.pm
@@ -0,0 +1,83 @@
+package Kutter::CLI::Filter;
+use Moose;
+use Carp;
+use Encode;
+use WebService::Simple;
+use WebService::Simple::Parser::XML::Simple;
+use XML::Simple;
+use YAML;
+use utf8;
+
+has 'appid' => ( is => 'rw', isa => 'Str', required => 1 );
+has 'service' => ( is => 'rw', isa => 'WebService::Simple', lazy_build => 1 );
+
+no Moose;
+
+sub _build_service {
+    my $self   = shift;
+    my $xs     = XML::Simple->new( ForceArray => [ 'Chunk', 'Morphem' ] );
+    my $parser = WebService::Simple::Parser::XML::Simple->new( xs => $xs );
+    return WebService::Simple->new(
+        base_url => 'http://jlp.yahooapis.jp/DAService/V1/parse',
+        param    => {
+            appid           => $self->appid,
+            response_parser => $parser,
+        }
+    );
+}
+
+sub parse {
+    my ( $self, $str ) = @_;
+    Carp::croak('Sentence Text is requred!') unless $str;
+    my $res = $self->service->get( { sentence => $str } );
+    my $ref = $res->parse_response();
+
+    my ( $eat_chunk_id, $food_name );
+
+    my @chunks = @{ $ref->{Result}{ChunkList}{Chunk} };
+    for my $chunk (@chunks) {
+        if ( $chunk->{MorphemList}{Morphem}[0]{Baseform} =~ /食べ/ ) {
+            $eat_chunk_id = $chunk->{Id};
+            last;
+        }
+        else {
+            my $count = 0;
+            for my $morphem ( @{ $chunk->{MorphemList}{Morphem} } ) {
+                if ( $morphem->{Baseform} =~ /食べ/ ) {
+                    while ($count) {
+                        $count--;
+                        if ( $chunk->{MorphemList}{Morphem}[$count]->{POS} =~
+                            /名詞/ )
+                        {
+                            $food_name .=
+                              $chunk->{MorphemList}{Morphem}[$count]{Baseform};
+                        }
+                        else {
+                            last;
+                        }
+                    }
+                    last;
+                }
+                $count++;
+            }
+        }
+    }
+
+    if ( $eat_chunk_id && !$food_name ) {
+        for my $chunk (@chunks) {
+            if ( $chunk->{Dependency} == $eat_chunk_id ) {
+                for my $morphem ( @{ $chunk->{MorphemList}{Morphem} } ) {
+                    if ( $morphem->{POS} =~ /名詞/ ) {
+                        $food_name .= $morphem->{Baseform};
+                    }
+                }
+            }
+        }
+    }
+
+    return $food_name;
+
+}
+
+1;
+__END__
---
+++ t/13_filter.t
@@ -0,0 +1,17 @@
+use strict;
+use Test::More;
+use Encode;
+use utf8;
+
+my $yahoo_appid = $ENV{YAHOO_APPID};
+if( ! $yahoo_appid ){
+    plan( skip_all => 'Please set set YAHOO_APPID to enable this test' );
+}else{
+    plan( tests => '2' );
+}
+
+use_ok('Kutter::CLI::Filter');
+my $filter = Kutter::CLI::Filter->new( appid => $yahoo_appid );
+my $str;
+$str = 'お腹がすいたので、カレーライスを食べた。';
+is( $filter->parse( $str ), 'カレーライス', encode('utf8', $str ) );
