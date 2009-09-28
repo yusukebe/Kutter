2223b00b9b81c0f3e958f1e943e32602e0c77406
Yusuke Wada <yusuke@kamawada.com>
2009-09-28 21:36:31 +0900

    use Kutter::Web::path_to on API::Role

--- lib/Kutter/API/Role.pm
+++ lib/Kutter/API/Role.pm
@@ -2,11 +2,12 @@ package Kutter::API::Role;
 use Moose::Role;
 use Path::Class;
 use FindBin;
+use Kutter::Web;
 
 #xxx
 sub path_to {
     my $file = shift;
-    return dir ( $file );
+    return Kutter::Web->path_to( $file );
 }
 
 1;
