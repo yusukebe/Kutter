f4f0193443b1ed599625a05c71c92df9072b321b
Yusuke Wada <yusuke@kamawada.com>
2009-09-29 14:25:38 +0900

    get an arg on Food controller

--- lib/Kutter/Web/Controller/Food.pm
+++ lib/Kutter/Web/Controller/Food.pm
@@ -4,39 +4,9 @@ use strict;
 use warnings;
 use parent 'Catalyst::Controller';
 
-=head1 NAME
-
-Kutter::Web::Controller::Food - Catalyst Controller
-
-=head1 DESCRIPTION
-
-Catalyst Controller.
-
-=head1 METHODS
-
-=cut
-
-
-=head2 index
-
-=cut
-
-sub index :Path :Args(0) {
-    my ( $self, $c ) = @_;
-
-    $c->response->body('Matched Kutter::Web::Controller::Food in Food.');
+sub index :Path :Args(1) {
+    my ( $self, $c, $arg ) = @_;
+    $c->stash->{template} = 'food.tt2';
 }
-
-
-=head1 AUTHOR
-
-yusuke
-
-=head1 LICENSE
-
-This library is free software. You can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=cut
 
 1;
