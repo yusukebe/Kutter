0f1ffcc022f2a4f766e528c1a3af479896d63a44
Yusuke Wada <yusuke@kamawada.com>
2009-09-29 12:36:17 +0900

    update crawler.pl with filtering

--- cli/crawler.pl
+++ cli/crawler.pl
@@ -5,8 +5,10 @@ use FindBin;
 use lib ( "$FindBin::Bin/../lib" );
 use Kutter::CLI::FeedReader;
 use Kutter::CLI::Crawler;
+use Kutter::CLI::Filter;
 
 my $feed_reader = Kutter::CLI::FeedReader->new();
+my $filter = Kutter::CLI::Filter->new( appid => 'your appid of YAHOO API');
 my $crawler = Kutter::CLI::Crawler->new( feed_reader => $feed_reader );
 
 $crawler->run();
