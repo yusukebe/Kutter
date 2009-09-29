30c64103851c163e9df34bbdd16f7f6f7e76244c
Yusuke Wada <yusuke@kamawada.com>
2009-09-29 14:00:17 +0900

    do perltidy

--- lib/Kutter/API.pm
+++ lib/Kutter/API.pm
@@ -5,14 +5,17 @@ use Kutter::API::DB;
 no Moose;
 
 sub get_recent_tweets {
-    my ($self, $page) = @_;
+    my ( $self, $page ) = @_;
     $page ||= 1;
     my $schema = Kutter::API::DB->schema;
-    return $schema->resultset('Tweet')->search({},{
-        rows => 20,
-        page => $page,
-        order_by => 'created_on DESC',
-    });
+    return $schema->resultset('Tweet')->search(
+        {},
+        {
+            rows     => 20,
+            page     => $page,
+            order_by => 'created_on DESC',
+        }
+    );
 }
 
 1;
