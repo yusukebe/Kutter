45190985f5ba05f8a184f3c00009ea1d6a054aa2
Yusuke Wada <yusuke@kamawada.com>
2009-10-11 17:48:31 +0900

    do perltidy

--- lib/Kutter/CLI/FeedReader.pm
+++ lib/Kutter/CLI/FeedReader.pm
@@ -6,17 +6,21 @@ use XML::Feed;
 use Kutter::CLI::FeedReader::Tweet;
 
 has 'url' => (
-    is  => 'rw',
-    isa => 'URI',
-    default =>
-      sub { URI->new( 'http://search.twitter.com/search.atom?q=%E9%A3%9F%E3%81%B9%E3%81%9F' ) },
+    is      => 'rw',
+    isa     => 'URI',
+    default => sub {
+        URI->new(
+           'http://search.twitter.com/search.atom?q=%E9%A3%9F%E3%81%B9%E3%81%9F'
+        );
+    },
 );
 
 no Moose;
 
 sub entries {
     my $self = shift;
-    my $feed = XML::Feed->parse( $self->url ) or Carp::croak ( XML::Feed->errstr );
+    my $feed = XML::Feed->parse( $self->url )
+      or Carp::croak( XML::Feed->errstr );
     return $feed->entries;
 }
 
@@ -24,8 +28,8 @@ sub tweets {
     my $self = shift;
     my @tweets;
     my @entries = $self->entries;
-    for my $entry ( @entries ){
-        push( @tweets, Kutter::CLI::FeedReader::Tweet->new( $entry ) );
+    for my $entry (@entries) {
+        push( @tweets, Kutter::CLI::FeedReader::Tweet->new($entry) );
     }
     return \@tweets;
 }
